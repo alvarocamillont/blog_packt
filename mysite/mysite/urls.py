e0fa2c491ed4d4d6b6d71e9b75c36bff20889282
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-09-29 08:22:30 -0300

    correção da url de entrada

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,7 +9,7 @@ sitemaps = {
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
+    url(r'', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^sitemap\.xml$' ,
         sitemap,
         {'sitemaps': sitemaps},
